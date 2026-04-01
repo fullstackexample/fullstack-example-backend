a6584301ffaa87c1c14e989685a4dd79d7ae6f3d
unknown <chaos1375@naver.com>
2026-04-01 09:02:22 +0900

    프로젝트 세팅

---
+++ src/main/java/com/queenanemone/fullstack_example_backend/FullstackExampleBackendApplication.java
@@ -0,0 +1,13 @@
+package com.queenanemone.fullstack_example_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullstackExampleBackendApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(FullstackExampleBackendApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/queenanemone/fullstack_example_backend/FullstackExampleBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.queenanemone.fullstack_example_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullstackExampleBackendApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
